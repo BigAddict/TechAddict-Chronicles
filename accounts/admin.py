e46eab5a7cab733ef12ece03e73bbb8fefa3ad45
Alemnew Marie <alemnewmarie461@gmail.com>
2023-02-06 09:11:49 +0300

    Email only login/signup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .forms import UserAdminChangeForm, UserAdminCreationForm
 
 User = get_user_model()
 
 
 @admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
     list_display = ["email", "first_name", "last_name", "is_superuser"]
     search_fields = ["email", "first_name", "last_name"]
