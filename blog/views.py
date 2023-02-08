dac7fa8b5d127c7989792735601a8bf16c32a362
Alemnew Marie <alemnewmarie461@gmail.com>
2023-02-08 12:13:32 +0300

    Post create feature with rich text field

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import View, ListView, DetailView
@@ -12,11 +14,8 @@ class HomePageView(View):
             return redirect(to=reverse("blog:post_list"))
 
         # popular posts and categories on landing page
-        featured_posts = (
-            Post.objects.select_related("category")
-            .select_related("author")
-            .filter(status=1)[:10]
-        )
+        featured_posts = Post.objects.all()[:10]
+
         popular_categories = Category.objects.all()[:6]
         return render(
             request,
@@ -47,8 +46,23 @@ class PostListView(ListView):
         return context
 
 
-class PostCreateView(CreateView):
-    pass
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """Display post creation form and handle the process."""
+
+    model = Post
+    fields = ["category", "title", "content", "status"]
+    template_name = "blog/post_create.html"
+
+    def form_valid(self, form):
+        # assign the current logged in user as author of the post
+        form.instance.author = self.request.user
+        if form.instance.status == 0:
+            msg = "Your post has been saved as draft."
+        elif form.instance.status == 1:
+            msg = "Your post has been published."
+
+        messages.success(self.request, msg)
+        return super().form_valid(form)
 
 
 class PostDetailView(DetailView):
