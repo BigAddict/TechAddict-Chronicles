3e8c8adf18b8cfc75f472566ce060067f4e10f5a
Alemnew Marie <alemnewmarie461@gmail.com>
2023-03-28 10:50:12 +0300

    Update blog tests

--- blog/tests.py
+++ blog/tests.py
@@ -2,89 +2,111 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Category, Post, Comment
+from .models import Category, Post
 
 User = get_user_model()
 
 
-class CategoryTests(TestCase):
-    def test_category(self):
+class HomePageTests(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse("blog:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/landing_page.html")
+        self.assertTrue("featured_posts" in response.context)
+        self.assertTrue("popular_categories" in response.context)
+
+
+class PostViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        number_of_posts = 13  # create 13 posts for pagination tests
         category = Category.objects.create(name="Test Category")
-        self.assertEqual(category.name, "Test Category")
-        self.assertEqual(len(category.post_set.all()), 0)
 
+        user = User.objects.create(username="testuser", email="testuser@email.com")
+        user.set_password("testpass123")
+        user.save()
 
-class PostTests(TestCase):
-    def setUp(self) -> None:
-        self.category = Category.objects.create(name="Test Category")
-        self.user = User.objects.create(
-            username="testuser", email="testuser@email.com", password="testpass123"
-        )
-        self.post = Post.objects.create(
-            title="This is test post",
-            content="This is post content",
-            author=self.user,
-            category=self.category,
-        )
+        for id in range(number_of_posts):
+            Post.objects.create(
+                title=f"Post title {id}",
+                content=f"Sample post content {id}",
+                status=1,
+                author=user,
+                category=category,
+            )
 
-    def test_post_create(self):
-        url = reverse("blog:post_create")
-        post_data = {
-            "title": "Post title",
-            "content": "Post content",
-            "category": self.category,
-            "author": self.user,
-        }
-        # user must be authenticated
-        response = self.client.post(url, data=post_data)
-        self.assertEqual(response.status_code, 302)
-
-        self.client.force_login(self.user)
-        response = self.client.post(url, data=post_data)
+    def test_post_list_view(self):
+        response = self.client.get("/stories/")
         self.assertEqual(response.status_code, 200)
-
-    def test_post_update(self):
-        update_url = reverse("blog:post_update", args=(self.post.slug,))
-        post_data = {
-            "id": 1,
-            "title": "This is test post (updated)",
-            "content": "This is post content (updated)",
-            "category": self.category,
-            "author": self.user,
-        }
-
-        # user must be authenticated
-        response = self.client.post(update_url, data=post_data)
-        self.assertEqual(response.status_code, 302)
-
-        user2 = User.objects.create(
-            username="User2", email="user2@email.com", password="testpass123"
-        )
-        # user must be authorized
-        self.client.force_login(user2)
-        response = self.client.post(update_url, data=post_data)
-        self.assertEqual(response.status_code, 403)
-
-        # owners can edit/update their own post
-        self.client.force_login(self.user)
-        response = self.client.post(update_url, data=post_data)
+        # accessible by name
+        response = self.client.get(reverse("blog:post_list"))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_list.html")
+        self.assertIn("saved_posts", response.context)
 
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse("blog:post_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context["is_paginated"] == True)
+        self.assertEqual(len(response.context["posts"]), 10)
 
-class CommentTests(TestCase):
-    def setUp(self) -> None:
-        self.category = Category.objects.create(name="Test Category")
-        self.user = User.objects.create(
-            username="testuser", email="testuser@email.com", password="testpass123"
+        # test pagination on category view
+        category = Category.objects.get(name="Test Category")
+        response = self.client.get(reverse("blog:category", args=[category.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/category.html")
+        self.assertTrue(response.context["is_paginated"] == True)
+        self.assertEqual(len(response.context["category_posts"]), 10)
+
+    def test_lists_all_posts(self):
+        # Get second page and confirm it has (exactly) remaining 3 items
+        response = self.client.get(reverse("blog:post_list") + "?page=2")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context["is_paginated"] == True)
+        self.assertEqual(len(response.context["posts"]), 3)
+
+        # test pagination on category view
+        category = Category.objects.get(name="Test Category")
+        response = self.client.get(
+            reverse("blog:category", args=[category.slug]) + "?page=2"
         )
-        self.post = Post.objects.create(
-            title="This is test post",
-            content="This is post content",
-            author=self.user,
-            category=self.category,
-        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context["is_paginated"] == True)
+        self.assertEqual(len(response.context["category_posts"]), 3)
 
-    def test_comment_create(self):
-        comment = Comment.objects.create(post=self.post, author=self.user)
-        self.assertEqual(comment.post, self.post)
-        self.assertNotEqual(self.post.comments.all(), [])
+    def test_post_detail_view(self):
+        post = Post.objects.get(id=1)
+        response = self.client.get(f"/p/{post.slug}/")
+        self.assertEqual(response.status_code, 200)
+        # accessible by name
+        response = self.client.get(reverse("blog:post_detail", args=[post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_detail.html")
+        self.assertTrue("saved_posts" in response.context)
+
+    def test_post_create_view(self):
+        # user must be logged in
+        response = self.client.get(reverse("blog:post_create"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"/accounts/login/?next=/p/new/")
+
+        login = self.client.login(email="testuser@email.com", password="testpass123")
+        self.assertTrue(login)
+
+        response = self.client.get(reverse("blog:post_create"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_create.html")
+
+    def test_post_update_view(self):
+        # user must be logged in
+        post = Post.objects.get(id=1)
+        response = self.client.get(reverse("blog:post_update", args=[post.slug]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"/accounts/login/?next=/p/{post.slug}/edit/")
+
+        login = self.client.login(email="testuser@email.com", password="testpass123")
+        self.assertTrue(login)
+
+        response = self.client.get(reverse("blog:post_update", args=[post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_update.html")
