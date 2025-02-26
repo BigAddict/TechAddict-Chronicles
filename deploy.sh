357b89d24716eb7fcef1c0248a4a75079629db34
bigaddict <davidnjihia536@gmail.com>
2025-02-26 14:13:52 +0300

    `Update deploy script and requirements to reflect changes in project structure and configuration.`

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate virtual environment
-source /home/ubuntu/django-blog/.venv/bin/activate
+source /home/ubuntu/TechAddict-Chronicles/venv/bin/activate
 export PYTHON=$(which python)
 
 # Run Django management commands
@@ -10,4 +10,4 @@ $PYTHON manage.py migrate
 $PYTHON manage.py collectstatic --noinput
 
 # Start Gunicorn (Modify as needed)
-exec gunicorn --access-logfile - --error-logfile - --workers 3 --bind unix:/run/gunicorn.sock django_blog.wsgi:application
+exec gunicorn --access-logfile - --error-logfile - --workers 3 --bind unix:/run/gunicorn.sock config.wsgi:application
